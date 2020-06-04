53999343277b3dda8ece33386afa93c2dcb1cf38
Reowlin <reowlin@gmail.com>
2020-06-04 11:34:20 +0300

    Update App.java

--- src/springhw/App.java
+++ src/springhw/App.java
@@ -6,11 +6,12 @@ import springhw.*;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.concurrent.Semaphore;
 
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
+        Semaphore sem = new Semaphore(1);
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the path to the files: ");
         String path = in.next();
@@ -19,10 +20,12 @@ public class App {
         ArrayList<Characters[]> arrayList = new ArrayList<>();
         //Characters[][] bob;
         for(int i = 0;i< Names.length; i++){
-            ParserThread myThread = new ParserThread(Names[i],arrayList, GoNames);
+            ParserThread myThread = new ParserThread(Names[i],arrayList, GoNames, sem);
             myThread.start();
         }
-        Thread.sleep(1000);
+        while (arrayList.size()<Names.length) {
+            Thread.sleep(100);
+        }
         Menu.first_menu(arrayList,GoNames);
     }
 
